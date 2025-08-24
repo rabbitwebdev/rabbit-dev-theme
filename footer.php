0c7f7e7096d241b560ee6869652ecd3eb7ee4739
pythedev <pyork@ihg.co.uk>
2025-08-24 21:09:12 +0100

    v13.0

--- footer.php
+++ footer.php
@@ -13,10 +13,12 @@
 
 
 
-  <script>
+ 
+  
+  <?php wp_footer(); ?>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+   <script>
     AOS.init();
   </script>
-  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-  <?php wp_footer(); ?>
 </body>
 </html>
