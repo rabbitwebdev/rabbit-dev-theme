878214d91413d423c64f6d7cd77482c035e0a46c
pythedev <pyork@ihg.co.uk>
2025-08-24 20:57:40 +0100

    v12.7

--- functions.php
+++ functions.php
@@ -16,6 +16,7 @@ function devrabbit_theme_enqueue_scripts() {
         wp_enqueue_style('swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', array(), '11.0', 'all');
 
          wp_enqueue_style('boot-icons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css', array(), '1.11.3', 'all');
+                  wp_enqueue_style('aos-style', 'https://unpkg.com/aos@2.3.1/dist/aos.css', array(), '2.3.1', 'all');
 
       wp_register_style( 'app', get_template_directory_uri() . '/dist/css/app.css', array(), '5.5', 'all' );
     wp_enqueue_style( 'app' );
@@ -25,6 +26,8 @@ function devrabbit_theme_enqueue_scripts() {
     wp_enqueue_script('bootstrap-pop', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js', array('jquery'), '2.9.2', true);
 
     wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js', array('jquery'), '5.3.2', true);
+
+    wp_enqueue_script('aos', 'https://unpkg.com/aos@2.3.1/dist/aos.js', array('jquery'), '2.3.1', true);
 
        wp_enqueue_script('js-swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array('jquery'), '2.9.2', true);
 
