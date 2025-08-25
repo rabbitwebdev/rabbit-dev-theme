0bff5653a5c16d9eab5db821320cd2c838b3adde
pythedev <pyork@ihg.co.uk>
2025-08-25 22:11:30 +0100

    v14.8

--- header.php
+++ header.php
@@ -5,24 +5,19 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
 <?php wp_head(); ?>
-
   <?php $head_code = get_field('head_code', 'options'); ?>
   <?php $body_code = get_field('body_code', 'options'); ?>
     <?php $woocoomerce_styles = get_field('woocoomerce_styles', 'options'); ?>
   <?php echo $head_code ; ?>
 </head>
-
 <body <?php body_class(); ?>>
   <?php echo $body_code ; ?>
 <main id="page" class="hfeed site woo-<?php echo $woocoomerce_styles ; ?>">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'devrabbittheme' ); ?></a>
-
 	    <header id="masthead" class="site-header  text-white" role="banner" >
             <div class="container-xl">
-                <div class="site-branding d-flex  justify-content-between flex-row align-items-center py-3">
-
+                <div class="site-branding d-flex  justify-content-between flex-row align-items-center py-2">
                 <?php
                 if ( has_custom_logo() ) {
                     the_custom_logo();
@@ -38,11 +33,8 @@
                     <?php endif;
                 }
                 ?>
-                
-
-                  <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+              <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
             </div>
             </div>
-
         </header><!-- #masthead -->
-        <div class="main-content">
+<div class="main-content">
