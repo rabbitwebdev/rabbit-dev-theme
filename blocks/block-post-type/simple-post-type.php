fa8e9e5f4772cb22eff999b0dcf8e5b1e8b0aa8a
pythedev <pyork@ihg.co.uk>
2025-09-06 16:54:29 +0100

    v15.6

--- blocks/block-post-type/simple-post-type.php
+++ blocks/block-post-type/simple-post-type.php
@@ -216,19 +216,17 @@ $alignimage = $style_data['align'];
 $select_featured_posts = get_field('select_featured_posts');
 if( $select_featured_posts ): ?>
     <div class="row row-cols-2">
-    <?php foreach( $select_featured_posts as $post ): 
-
-        // Setup this post for WP functions (variable must be named $post).
-        setup_postdata($post); ?>
+    <?php foreach( $select_featured_posts as $featured_post ): 
+   $permalink = get_permalink( $featured_post->ID );
+        $title = get_the_title( $featured_post->ID );
+        $custom_field = get_field( 'field_name', $featured_post->ID );
+        ?>
         <div class="col">
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            <span>A custom field from this post: <?php the_field( 'field_name' ); ?></span>
+            <a href="<?php echo esc_url( $permalink ); ?>"><?php echo esc_html( $title ); ?></a>
+            <span>A custom field from this post: <?php echo esc_html( $custom_field ); ?></span>
         </div>
     <?php endforeach; ?>
     </div>
-    <?php 
-    // Reset the global post object so that the rest of the page works correctly.
-    wp_reset_postdata(); ?>
 <?php endif; ?>
     
 </div> <!-- Block end -->
